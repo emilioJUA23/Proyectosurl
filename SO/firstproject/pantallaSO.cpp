9ef8600b56420260a4565da418d795bb55b45a20
Luis Emilio Juarez Juarez <Luis Emilio Juarez Juarez>
2017-03-14 19:48:24 -0600

    este commit hace cambios en el progreso del di- senio de la pantalla de SO fisrtproject

--- SO/firstproject/pantallaSO.cpp
+++ SO/firstproject/pantallaSO.cpp
@@ -30,18 +30,41 @@ void  pantalla_inicial()
 
 
 
+string squares[6][9];	
+void llenar_matriz()
+{
+	for(int i=0;i<6;i=i+1)
+	{
+	  for(int j=0;j<9;j=j+1)
+		{
+		  squares[i][j]=string(15,' ');	
+		} 	
+	}
+}
 int main()
 {
-  	pantalla_inicial();
+  	//pantalla_inicial();
   	//getchar();
   	//system("cls");
-  	string line(15,'*');
-  	string square[] = {line,line,line,line,line,line,line,line,line};
-  	square[3][5]='5';
-	for(int i=0;i<9;i=i+1)
+  	//printf("\033[%d;%dH", row, col);
+  	
+  	//string line(15,' ');
+  	//string square[] = {line,line,line,line,line,line,line,line,line};
+	llenar_matriz();
+	squares[1][2][4]='*';
+	squares[1][3][4]='?';
+	squares[1][2][5]='0';
+	squares[2][2][4]='*';
+	for(int i=0;i<6;i=i+1)
 	{
-	  cout <<square[i]<<endl;	
-	} 
+	  for(int j=0;j<9;j=j+1)
+		{
+		  	cout <<squares[i][j]<<endl;
+		} 	
+		cout <<endl;
+		cout <<endl;
+	}
+
 	getchar();
 	system("cls");
   	return 0;
