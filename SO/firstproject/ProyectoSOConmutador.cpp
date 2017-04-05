1915d5318fdfb29aadb08ddb4918d1a72e84d4c2
emilioJUA23 <luis.juarez2394@gmail.com>
2017-04-04 20:51:32 -0600

    Blinkeo agregado y estructura pcb

--- SO/firstproject/ProyectoSOConmutador.cpp
+++ SO/firstproject/ProyectoSOConmutador.cpp
@@ -11,25 +11,37 @@
 	#define __CPPARGS
 #endif
 
+struct PCB
+{
+	int estado; //Sin ejecucion = 0, En ejecucion = 1, En pausa = 2
+	int quantum;
+	int color;
+	char square[9][15];
+};
+
 void interrupt (*oldhandler)(__CPPARGS);
 
 //Variables Globales
-int count = 0;
-int quantum = 0;
 int tempo = 0;
 int m_presionado = 0;
 int contexto = 0;
 int pt_cadena = 0; //Size de cadena
 char cadena[80]; //Comandos
 int salir = 0;
-char squares[6][9][15];//squares[pantalla][posicion y][posicion x]
-int  habilitados [6];
-char far *video_mem = (char far*) 0xBB8000000L;
+PCB squares[6];//squares[pantalla][posicion y][posicion x]
 
 
 void limpiar_texto()
 {
 	gotoxy(2,23);
+	for(int q=0;q<(17*3)-2;q=q+1)
+	{
+		 printf(" ");
+	}
+}
+void limpiar_respuesta()
+{
+	gotoxy(2,25);
 	for(int q=0;q<(17*3)-2;q=q+1)
 	{
 		 printf(" ");
@@ -67,20 +79,20 @@ void  pantalla_inicial()
 			}
 			printf("*");
 			printf("\n");
-	    }
-	    for(int m=0;m<17*3;m=m+1)
+		}
+		for(int m=0;m<17*3;m=m+1)
 		{
 			 printf("*");
 		}
 		printf("\n");
-		
+
 	}
 	printf("*");
 	for(int q=0;q<(17*3)-2;q=q+1)
 	{
 		 printf(" ");
 	}
-	 printf("*");
+	printf("*");
 	printf("\n");
 	for(int r=0;r<17*3;r=r+1)
 	{
@@ -101,7 +113,7 @@ int posx(int pantallax)
 
 int posy(int pantallay)
 {	//metodo que devuelve la posicion y inicial de la pantalla
-    if (pantallay==1||pantallay==2||pantallay==3)	{return 2;}
+	if (pantallay==1||pantallay==2||pantallay==3)	{return 2;}
 	else if (pantallay==4||pantallay==5||pantallay==6){return 2+11;}
 	else{return 0;}
 }
@@ -110,30 +122,37 @@ void imprimir_pantalla(int pa)
 {
    for (int i = 0; i < 9; ++i)
    {
-   		for (int j = 0; j < 15; ++j)
-   		{
-   			gotoxy(posx(pa)+j,posy(pa)+i);
-   			printf("%c", squares[pa-1][i][j]);
-   		}
+		for (int j = 0; j < 15; ++j)
+		{
+			gotoxy(posx(pa+1)+j,posy(pa+1)+i);
+			if(squares[pa].color == 0)
+			{
+			   printf("%c", squares[pa].square[i][j]);
+			}
+			else
+			{
+			   printf(" ");
+			}
+		}
    }
-
+   squares[pa].color = (squares[pa].color+1)%2;
 }
 
 int tell_me_a_screen(int x,int y)
 {
 	//pantalla 1
 	if (x>1&&x<17&&y>1&&y<11){return 1;}
-	//pantalla 2 
+	//pantalla 2
 	else if (x>18&&x<34&&y>1&&y<11){return 2;}
-	//pantalla 3 
+	//pantalla 3
 	else if (x>35&&x<51&&y>1&&y<11){return 3;}
-	//pantalla 4 
+	//pantalla 4
 	else if (x>1&&x<17&&y>12&&y<22){return 4;}
 	//pantalla 5
 	else if (x>18&&x<34&&y>12&&y<22){return 5;}
 	//pantalla 6
 	else if (x>35&&x<51&&y>12&&y<22){return 6;}
-  	else {return 0;}
+	else {return 0;}
 
 }
 
@@ -148,7 +167,7 @@ int ResetRaton()
 		printf("No hat un mouse instalado \n");
 		return -1;
 	}
-	else  
+	else
 	{
 		//printf("Raton listo para utilizarse\n");
 		regin.x.ax = 1;
@@ -170,22 +189,19 @@ int ClicRaton()
 		int pantalla = tell_me_a_screen(x,y);
 		if (pantalla==0)
 		{
-			count--;
+
 		}
 		else
-		{ 
-			
-			if (habilitados[pantalla-1]==1)
+		{
+
+			if (squares[pantalla-1].estado==1)
 			{
 			   gotoxy(x,y);
-			   printf("#");
-			   squares[pantalla-1][y-posy(pantalla)][x-posx(pantalla)]='#';
+			   printf(".");
+			   squares[pantalla-1].square[y-posy(pantalla)][x-posx(pantalla)]='.';
 			}
-			
-	      //  imprimir_pantalla(pantalla);
 		}
 		m_presionado = 1;
-		count++;
 		return 1;
 	 }
 	 if((int)regout.x.bx == 0)
@@ -200,7 +216,7 @@ int ClicRaton()
 //---------------------PROCESO DE TECLADO---------------------------
 void comando()
 {
-	char *add = "add", *pause = "pause", *play = "play", *clear ="clear", *quantum = "quantum", *stats = "stats", *erase = "erase";
+	char *exit = "exit", *add = "add", *pause = "pause", *play = "play", *clear ="clear", *quantum = "quantum", *stats = "stats", *erase = "erase";
 	int cmp;
 	char *ptr, *tmp;
 	char space = ' ';
@@ -213,60 +229,77 @@ void comando()
 	if (cmp == 0)
 	{
 		/* codigo add */
+		limpiar_respuesta();
 	}
 	else
 	{
-	    cmp = strncmp(cadena,pause,5);
-	    if (cmp == 0)
-	    {
-		/* codigo pause */
-	    }
-	    else
-	    {
-		cmp = strncmp(cadena,play,4);
+		cmp = strncmp(cadena,pause,5);
 		if (cmp == 0)
 		{
-		     /* codigo play */
+			/* codigo pause */
+			limpiar_respuesta();
 		}
 		else
 		{
-		    cmp = strncmp(cadena,clear,5);
-		    if (cmp == 0)
-		    {
-			/* codigo clear */
-		    }
-		    else
-		    {
-			cmp = strncmp(cadena,quantum,7);
+			cmp = strncmp(cadena,play,4);
 			if (cmp == 0)
 			{
-			    tmp=strrchr(cadena,space);
-			    tmp++;
-			    /* codigo quantum */
+				 /* codigo play */
+				 limpiar_respuesta();
 			}
 			else
 			{
-			    cmp = strncmp(cadena,stats,5);
-			    if (cmp == 0)
-			    {
-				/* codigo stats */
-			    }
-			    else
-			    {
-				cmp = strncmp(cadena,erase,5);
+				cmp = strncmp(cadena,clear,5);
 				if (cmp == 0)
 				{
-				    /* codigo erase */
+					/* codigo clear */
+					limpiar_respuesta();
 				}
 				else
 				{
-				    printf("Comando invalido\n");
+					cmp = strncmp(cadena,quantum,7);
+					if (cmp == 0)
+					{
+						tmp=strrchr(cadena,space);
+						tmp++;
+						/* codigo quantum */
+						limpiar_respuesta();
+					}
+					else
+					{
+						cmp = strncmp(cadena,stats,5);
+						if (cmp == 0)
+						{
+							/* codigo stats */
+							limpiar_respuesta();
+						}
+						else
+						{
+							cmp = strncmp(cadena,erase,5);
+							if (cmp == 0)
+							{
+								/* codigo erase */
+								limpiar_respuesta();
+							}
+							else
+							{
+								cmp = strncmp(cadena,exit,4);
+								if (cmp == 0)
+								{
+									/* codigo exit */
+									limpiar_respuesta();
+									salir = 1;
+								}
+								else
+								{
+									printf("Comando invalido\n");
+								}
+							}
+						}
+					}
 				}
-			    }
 			}
-		    }
 		}
-	    }
 	}
 }
 int Teclado(void)
@@ -320,18 +353,10 @@ int Teclado(void)
 void interrupt temporizador(__CPPARGS)
 {
 	disable();
-	if(tempo == quantum)
+	if(tempo == squares[contexto].quantum)
 	{
-		//color_text();
-		if(contexto == 0)
-		{
-			contexto = 1;
-		}
-		else if (contexto==1)
-		{
-			contexto=2;
-		}
-		else
+		contexto++;
+		if(contexto > 5)
 		{
 			contexto = 0;
 		}
@@ -349,12 +374,14 @@ void interrupt temporizador(__CPPARGS)
 //---------------------PROCESO KERNEL-------------------------------
 int main(void)
 {
-	int zahl=1;	
+	_setcursortype(_NOCURSOR);
 	for (int j = 0; j < 6; ++j)
 	{
-		habilitados[j]=1;
+		squares[j].estado=1;
+		squares[j].color=0;
+		squares[j].quantum=0;
 	}
-	
+
 	clrscr();
 	//llamamos a la pintada inicial
 	pantalla_inicial();
@@ -362,32 +389,16 @@ int main(void)
 	oldhandler = getvect(INTR);
 	setvect(INTR, temporizador);
 	gotoxy(2,23);
-	while ((count < 15) && (salir == 0))
+	while (salir == 0)
 	{
-		switch(contexto)
-		{
-			case 0:
-				ClicRaton();
-				break;
-			case 1:
-				Teclado();
-				break;
-			case 2:
-				 if (habilitados[zahl-1]==1)
-				 {
-				 	imprimir_pantalla(zahl);
-				 }
-			     zahl++;
-			     if (zahl==7)
-			     {
-			     	zahl=1;
-			     }
-			break;
-		}
+		ClicRaton();
+		Teclado();
+		imprimir_pantalla(contexto);
 	}
 	setvect(INTR, oldhandler);
 	limpiar_texto();
-	gotoxy(2,23);
+	limpiar_respuesta();
+	gotoxy(2,25);
 	printf("Presiona enter para salir");
 	gets(cadena);
 	return 0;
